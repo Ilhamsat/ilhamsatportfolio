bf665416e8b142be2600cd6f30d4bd86ce4be046
Ilhamsat <ilhamsatriadi10@gmail.com>
2022-10-12 21:34:48 +0700

    feat(main): Add desc and icon connect
    Add short desc and icon connect

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import {FaGithub, FaLinkedinIn} from "react-icons/fa";
+import {AiOutlineMail} from "react-icons/ai";
+import {BsFillPersonLinesFill} from "react-icons/bs";
 
 const Main = () => {
     return (
@@ -16,6 +19,25 @@ const Main = () => {
                     <h1 className='py-2 text-gray-700'>
                        A Full-Stack Developer
                     </h1>
+                    <p className='py-4 text-gray-600 max-w-[70%] m-auto'>
+                        I am interested in becoming a Backend Developer especially with the Go programming language.
+                        And I want to learn more in theory, practice, and case studies related to Backend Developer.
+                        I like to explore and learn new things related to programming, and I also highly adaptive with new frameworks and technologies.
+                    </p>
+                    <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+                        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                            <FaLinkedinIn/>
+                        </div>
+                        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                            <FaGithub/>
+                        </div>
+                        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                            <AiOutlineMail/>
+                        </div>
+                        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                            <BsFillPersonLinesFill/>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
