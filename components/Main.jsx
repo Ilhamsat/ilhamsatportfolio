3433301eb5fd7d2a15c6c3203418516d5ddf9fd5
Ilhamsat <ilhamsatriadi10@gmail.com>
2022-10-12 16:42:44 +0700

    fix(app): fix add components and logo
    Add components, logo, and tailwind

---
+++ components/Main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Main = () => {
+    return (
+        <div className='w-full h-screen text-center'>
+            <div className='max-w[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+                <div>
+                    <p className='uppercase text-sm tracking-widest text-gray-600'>
+                        {/* eslint-disable-next-line react/no-unescaped-entities */}
+                        Let's build something great together
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main
